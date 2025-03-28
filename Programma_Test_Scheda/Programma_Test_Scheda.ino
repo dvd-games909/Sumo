2f92321c3ee2da9e9d35759202c44e450ad08470
dvd-games909 <120468503+dvd-games909@users.noreply.github.com>
2025-03-28 18:52:55 +0100

    Caricamento file di test per scheda CNY70
    Questo programma va utilizzato per controllare il corretto funzionamento della scheda utilizzando un campione per il colore nero e uno per il colore bianco

---
+++ Programma_Test_Scheda/Programma_Test_Scheda.ino
@@ -0,0 +1,21 @@
+/*Programma di test per la scheda CNY70
+Questo programma fa un test del riconoscimento del bianco e del nero e della variazione di potenza dei CNY70
+*/
+#define CNY0 A0
+#define CNY1 A1
+#define CNY2 A2
+#define potenza 10
+void setup() {
+Serial.begin(9600);
+pinMode(10,OUTPUT);
+Serial.println("PROGRAMMA DI TEST SCHEDA CNY70");
+delay(2000);
+Serial.println("Preparare i campioni di colori per il test e ricordarsi di mantenere i sensori ad una distanza di circa 1mm dal campione");
+delay(5000);
+Serial.println("PROVA BIANCO");
+Serial.println("")
+}
+
+void loop() {
+
+}
