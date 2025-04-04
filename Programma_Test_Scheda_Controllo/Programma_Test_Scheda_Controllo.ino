a814650feb7b79b5996cf2c1a742808388f2ca85
dvd-games909 <120468503+dvd-games909@users.noreply.github.com>
2025-04-04 14:27:23 +0200

    Aggiunto Programma_Test_Scheda_Controllo

---
+++ Programma_Test_Scheda_Controllo/Programma_Test_Scheda_Controllo.ino
@@ -0,0 +1,12 @@
+/*
+Programma di test per il funzionamento scheda di controllo del robot SUMO
+Questo programma programma verifica il funzionamento del circuito integrato L293D mobilitando i motori
+e verifica il funzionamento dei componenti connessi sulla scheda
+*/
+void setup() {
+
+}
+
+void loop() {
+
+}
